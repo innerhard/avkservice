331b724834319bd027d88b5cecaabb5c9f317ab7
Innerhard <innerhard@gmail.com>
2022-01-31 21:24:46 +0300

    test

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,23 @@
+import styled from 'styled-components'
+import { LayoutPage } from '../src'
+import { NotFound } from '../src/icons/404'
+import { Typography } from '@mui/material'
+
 export default function Custom404() {
-    return <h1>404 - FUCK what</h1>
+    return (
+        <LayoutPage>
+            <WrapperAddress>
+                <Typography variant="h3" style={{ justifySelf: 'center' }}>
+                    Страница не найдена!
+                </Typography>
+                <NotFound />
+            </WrapperAddress>
+        </LayoutPage>
+    )
 }
+
+const WrapperAddress = styled.div`
+    display: grid;
+    min-height: 1024px; 
+    bottom: 0;
+`
--- src/api/query.ts
+++ src/api/query.ts
@@ -16,31 +16,31 @@ export const getPhones = () => axios.get('https://dev.avksrv.ru/callphones')
 export const getStocks = () => axios.get('https://dev.avksrv.ru/all-stocks/')
 export const putPhones = (id: string | number, data: any) => axios.put(`https://dev.avksrv.ru/callphones/${id}`, data)
 
-export const queryAPI = (
-    link: string,
-    // setData: { (value: React.SetStateAction<null>): void; (arg0: any): void },
-    // setError: (value: boolean) => void,
-) => {
-    getAPI.then(item => {
-        if (item.status === 200) {
-            axios
-                .get(link, {
-                    headers: { Authorization: `Bearer ${item?.data?.jwt}` },
-                })
-                .then(({ data, status }) => {
-                    console.log(data)
-                    if (status === 200) {
-                        // setData(data)
-                    } else {
-                        // setError(true)
-                    }
-                })
-                .catch(() => {
-                    // setError(true)
-                })
-        }
-    })
-}
+// export const queryAPI = (
+//     link: string,
+//     // setData: { (value: React.SetStateAction<null>): void; (arg0: any): void },
+//     // setError: (value: boolean) => void,
+// ) => {
+//     getAPI.then(item => {
+//         if (item.status === 200) {
+//             axios
+//                 .get(link, {
+//                     headers: { Authorization: `Bearer ${item?.data?.jwt}` },
+//                 })
+//                 .then(({ data, status }) => {
+//                     console.log(data)
+//                     if (status === 200) {
+//                         // setData(data)
+//                     } else {
+//                         // setError(true)
+//                     }
+//                 })
+//                 .catch(() => {
+//                     // setError(true)
+//                 })
+//         }
+//     })
+// }
 //
 // export const queryStaticData = async (link: string) => {
 //     return getAPI.then(item => {
