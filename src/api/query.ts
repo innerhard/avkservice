80dbd8d6a6f6c61cca0b7c37763c88d53f446be6
Innerhard <innerhard@gmail.com>
2022-04-17 23:04:05 +0300

    Ðadd env.process

--- src/api/query.ts
+++ src/api/query.ts
@@ -1,24 +1,25 @@
 import axios from 'axios'
 
-export const getAPI = axios.post('/admin/login', {
+const getPath = () => (process.env.NODE_ENV === 'development' ? 'https://dev.avksrv.ru' : 'https://avksrv.ru')
+export const getAPI = axios.post(getPath() + '/admin/login', {
     email: 'innerhard@gmail.com',
     password: 'WhoFuckMe!100505',
 })
 
 export const getAuth = (login: string, password: string) =>
-    axios.post('/admin/login', {
+    axios.post(getPath() + '/admin/login', {
         email: login,
         password: password,
     })
 
-export const getCategories = () => axios.get('/category-services')
-export const getGlobalInfo = () => axios.get('/global')
-export const getPhones = () => axios.get('/callphones')
-export const getStocks = () => axios.get('/all-stocks/')
-export const getCardsBlocks = () => axios.get('/cards-blocks/')
-export const getCameraBlocks = () => axios.get('/camera-services/')
-export const getWidgetServices = () => axios.get('/widget-services/')
-export const putPhones = (id: string | number, data: any) => axios.put(`/callphones/${id}`, data)
+export const getCategories = () => axios.get(getPath() + '/category-services')
+export const getGlobalInfo = () => axios.get(getPath() + '/global')
+export const getPhones = () => axios.get(getPath() + '/callphones')
+export const getStocks = () => axios.get(getPath() + '/all-stocks/')
+export const getCardsBlocks = () => axios.get(getPath() + '/cards-blocks/')
+export const getCameraBlocks = () => axios.get(getPath() + '/camera-services/')
+export const getWidgetServices = () => axios.get(getPath() + '/widget-services/')
+export const putPhones = (id: string | number, data: any) => axios.put(getPath() + `/callphones/${id}`, data)
 
 // export const queryAPI = (
 //     link: string,
